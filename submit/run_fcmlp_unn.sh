f07e92d21a26f35edc158865c83e7a23a36cc458
Archie Hewitt <hewitt.archie@yandex.ru>
2021-09-07 19:19:44 +0300

    Added gres

--- submit/run_fcmlp_unn.sh
+++ submit/run_fcmlp_unn.sh
@@ -4,6 +4,7 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --time=72:00:00
 #SBATCH --partition=a100
+#SBATCH --gres=gpu:2
 #SBATCH -o /common/home/yusipov_i/source/dnam/submit/output/%j.txt
 
 module load cuda/cuda-11.3
@@ -11,7 +12,7 @@ export CUDA_VISIBLE_DEVICES=0
 
 code_dir=/common/home/yusipov_i/source/dnam
 
-printf "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
+printf "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES \n"
 printf "args:\n $1 \n"
 
 srun python $code_dir/run_fcmlp.py $1
