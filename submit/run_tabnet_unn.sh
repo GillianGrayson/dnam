3c872c3bce08b361408e9bc5cfd9e16867e823cc
Archie Hewitt <hewitt.archie@yandex.ru>
2021-09-07 19:06:26 +0300

    again

--- submit/run_tabnet_unn.sh
+++ submit/run_tabnet_unn.sh
@@ -3,17 +3,15 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
 #SBATCH --time=72:00:00
-#SBATCH --partition=gpu
+#SBATCH --partition=a100
 #SBATCH -o /common/home/yusipov_i/source/dnam/submit/output/%j.txt
 
 module load cuda/cuda-11.3
 export CUDA_VISIBLE_DEVICES=0
 
-cudaDevs=$(echo $CUDA_VISIBLE_DEVICES | sed -e 's/,/ /g')
-printf "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
-
 code_dir=/common/home/yusipov_i/source/dnam
 
+printf "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
 printf "args:\n $1 \n"
 
 srun python $code_dir/run_tabnet.py $1
