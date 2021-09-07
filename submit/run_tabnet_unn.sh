6d7c7951feeb7179aa8540a24c39545cbba5c7c6
Archie Hewitt <hewitt.archie@yandex.ru>
2021-09-07 18:13:59 +0300

    again

--- submit/run_tabnet_unn.sh
+++ submit/run_tabnet_unn.sh
@@ -7,7 +7,7 @@
 #SBATCH -o /common/home/yusipov_i/source/dnam/submit/output/%j.txt
 
 module load cuda/cuda-11.3
-#export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=0
 
 cudaDevs=$(echo $CUDA_VISIBLE_DEVICES | sed -e 's/,/ /g')
 printf "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
