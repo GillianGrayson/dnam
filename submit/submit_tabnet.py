c0e14b47eb3d1e966790c976edbddeaeb1946a21
Archie Hewitt <hewitt.archie@yandex.ru>
2021-09-07 16:15:00 +0300

    again

--- submit/submit_tabnet.py
+++ submit/submit_tabnet.py
@@ -1,5 +1,6 @@
 import os
 
+project_name = 'tabnet_1'
 
 data_path = "/home/yusipov_i/data/dnam/datasets/meta/BrainDiseases/variance_0.005"
 
@@ -17,6 +18,7 @@ optimizer_weight_decay = [0.0, 0.0001, 0.001]
 args = f"--multirun hparams_search=tabnet " \
        f"experiment=tabnet work_dir=\"{data_path}/models/tabnet\" " \
        f"data_dir=\"{data_path}\" " \
-       f"datamodule.path=\"{data_path}\""
+       f"datamodule.path=\"{data_path}\" " \
+       f"project_name: {project_name}"
 
 os.system(f"sbatch run_tabnet_unn.sh \"{args}\"")
