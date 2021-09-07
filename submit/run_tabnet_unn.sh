8fba1094a2101a07509c4fa8c7e2dc4a62f45304
Archie Hewitt <hewitt.archie@yandex.ru>
2021-09-07 21:55:43 +0300

    partition is needed

--- submit/run_tabnet_unn.sh
+++ submit/run_tabnet_unn.sh
@@ -3,6 +3,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
 #SBATCH --time=72:00:00
+#SBATCH --partition=a100
 #SBATCH --gres=gpu:1
 #SBATCH -o /common/home/yusipov_i/source/dnam/submit/output/%j.txt
 
