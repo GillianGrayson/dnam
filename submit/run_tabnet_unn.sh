7b27d6c975105ac1141f08d713d10234ced13e1e
Archie Hewitt <hewitt.archie@yandex.ru>
2021-09-07 15:48:45 +0300

    again

--- submit/run_tabnet_unn.sh
+++ submit/run_tabnet_unn.sh
@@ -4,8 +4,8 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --time=72:00:00
 #SBATCH --partition=gpu
-#SBATCH -o /common/home/yusipov_i/source/dnam/output/%j.txt
-#SBATCH -e /common/home/yusipov_i/source/dnam/errors/%j.txt
+#SBATCH -o /common/home/yusipov_i/source/dnam/submit/output/%j.txt
+#SBATCH -e /common/home/yusipov_i/source/dnam/submit/errors/%j.txt
 
 code_dir=/common/home/yusipov_i/source/dnam
 
