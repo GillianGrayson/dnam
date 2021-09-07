4170e9fc9d2e743bea5de39978c4c3a6d0becfcf
Archie Hewitt <hewitt.archie@yandex.ru>
2021-09-07 21:50:44 +0300

    Deleted partition

--- submit/run_fcmlp_unn.sh
+++ submit/run_fcmlp_unn.sh
@@ -3,12 +3,11 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
 #SBATCH --time=72:00:00
-#SBATCH --partition=a100
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH -o /common/home/yusipov_i/source/dnam/submit/output/%j.txt
 
 code_dir=/common/home/yusipov_i/source/dnam
 
-printf "args:\n $1 \n"
+printf "args:\n $1 \n\n"
 
 srun python $code_dir/run_fcmlp.py $1
