14d27f74e3306ad14cffa982dbe4e52d8a4001d9
Irog Yusipov <yusipov_i@master.nngu>
2021-09-10 23:24:48 +0300

    tabnet_3 from unn

--- submit/wandb_sync.py
+++ submit/wandb_sync.py
@@ -1,8 +1,8 @@
 import os
 from glob import glob
 
-model = "tabnet_2"
-date_time = "2021-09-08_17-34-29"
+model = "tabnet_3"
+date_time = "2021-09-09_00-41-41"
 folder_path = f"/common/home/yusipov_i/data/dnam/datasets/meta/BrainDiseases/variance_0.005/models/{model}/logs/multiruns/{date_time}"
 
 folders_to_sync = glob(f"{folder_path}/*/wandb/offline-run-*")
