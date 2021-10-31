c9d4c2201786f8558b536642bdf851e887847691
Irog Yusipov <yusipov_i@master.nngu>
2021-10-31 23:07:57 +0300

    unn lightgbm run

--- submit/wandb_sync.py
+++ submit/wandb_sync.py
@@ -1,12 +1,12 @@
 import os
 from glob import glob
 
-model = "tabnetpl_unnhpc_0.005_200"
+model = "lightgbm_unnhpc"
 
 check_sum = 'd11b5f9b6efd089db42a3d5e6b375430'
-input_dim = 200
+input_dim = 375614
 output_dim = 6
-date_time = "2021-10-24_17-07-35"
+date_time = "2021-10-29_14-05-03"
 data_path = f"/home/yusipov_i/data/dnam/datasets/meta/{check_sum}/models/{model}/logs/multiruns/{date_time}"
 
 project = f"{input_dim}_{output_dim}_{check_sum}_{date_time}"
