bf979723f8dd33121e4e3271d8dbabc0a5d0a4d0
Archie Hewitt <hewitt.archie@yandex.ru>
2021-12-02 06:16:27 +0300

    done 008

--- scripts/python/dataset_specific/GSEUNN/tasks/008_H72_9_points.py
+++ scripts/python/dataset_specific/GSEUNN/tasks/008_H72_9_points.py
@@ -2,6 +2,7 @@ import pandas as pd
 from scripts.python.routines.manifest import get_manifest
 import numpy as np
 import os
+import plotly.express as px
 from scripts.python.pheno.datasets.filter import filter_pheno, get_passed_fields
 from scipy.stats import spearmanr
 import matplotlib.pyplot as plt
@@ -46,12 +47,10 @@ betas = pd.read_pickle(f"{path}/{platform}/{dataset}/betas.pkl")
 
 df = pd.merge(pheno, betas, left_index=True, right_index=True)
 
-
 fig = go.Figure()
 for cpg_id, cpg in enumerate(target_cpgs):
 
     all_betas = df.loc[:, cpg].values
-
     fig.add_trace(
         go.Violin(
             x=[cpg]*len(all_betas),
@@ -60,14 +59,13 @@ for cpg_id, cpg in enumerate(target_cpgs):
             meanline_visible=True,
             line_color='blue',
             showlegend=False,
-            opacity=0.7
+            opacity=1.0
         )
     )
 
     showlegend = False
     if cpg_id == 0:
         showlegend = True
-
     base_beta = df.loc[df['ID'] == 'H72', cpg].values[0]
     fig.add_trace(
         go.Scatter(
@@ -87,6 +85,28 @@ for cpg_id, cpg in enumerate(target_cpgs):
         )
     )
 
+    colors = px.colors.qualitative.Set1
+    for ver_id, ver in enumerate(["ver_1", "ver_2", "ver_3"]):
+        ver_beta = H72.loc[cpg, ver] * 0.01
+        fig.add_trace(
+            go.Scatter(
+                x=[cpg],
+                y=[ver_beta],
+                showlegend=showlegend,
+                name=ver,
+                mode="markers",
+                marker=dict(
+                    size=12,
+                    opacity=0.7,
+                    line=dict(
+                        width=1
+                    ),
+                    color=colors[ver_id + 2]
+                ),
+            )
+        )
+
+
 add_layout(fig, "", 'Methylation level', f"")
 fig.update_xaxes(tickangle=270)
 fig.update_xaxes(tickfont_size=15)
@@ -98,35 +118,3 @@ fig.update_layout(margin=go.layout.Margin(
     pad=0
 ))
 save_figure(fig, f"{path_save}/figs/tmp")
-
-
-
-df_case = df.loc[(df[status_col] == 'ESRD'), :]
-
-for feat in features:
-    df_case[feat] = StandardScaler().fit_transform(np.array(df_case[feat]).reshape(-1, 1))
-
-df_case['HET'] = 0
-
-for feat in features:
-    tmp_1 = df_case[feat].values - np.mean(df_case[feat].values)
-    tmp_2 = np.power(tmp_1, 2)
-    df_case['HET'] += np.power((df_case[feat].values - np.mean(df_case[feat].values)), 2)
-
-df_outcome_alive = df_case.loc[(df_case['Disease_outcome'] == 'alive'), :]
-df_outcome_dead = df_case.loc[(df_case['Disease_outcome'] == 'dead'), :]
-
-statistic_outcome, pvalue_outcome = mannwhitneyu(df_outcome_alive['HET'].values, df_outcome_dead['HET'].values)
-box_outcome = go.Figure()
-add_box_trace(box_outcome, df_outcome_alive['HET'].values, f"Alive ({df_outcome_alive.shape[0]})")
-add_box_trace(box_outcome, df_outcome_dead['HET'].values, f"Dead ({df_outcome_dead.shape[0]})")
-add_layout(box_outcome, "", 'v', f"Mann-Whitney p-value: {pvalue_outcome:0.2e}")
-box_outcome.update_layout({'colorway': ['blue', 'red']})
-save_figure(box_outcome, f"{path_save}/figs/box/outcome_HET")
-vio_outcome = go.Figure()
-add_violin_trace(vio_outcome, df_outcome_alive['HET'].values, f"Alive ({df_outcome_alive.shape[0]})")
-add_violin_trace(vio_outcome, df_outcome_dead['HET'].values, f"Dead ({df_outcome_dead.shape[0]})")
-add_layout(vio_outcome, "", "v", f"Mann-Whitney p-value: {pvalue_outcome:0.2e}")
-vio_outcome.update_layout({'colorway': ['blue', 'red']})
-vio_outcome.update_traces(points='all', pointpos=0)
-save_figure(vio_outcome, f"{path_save}/figs/vio/outcome_{'HET'}")
