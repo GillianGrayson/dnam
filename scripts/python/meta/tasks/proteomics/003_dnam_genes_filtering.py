357c1e2f925a687fbd1bcc919c67a3c65f51bc54
Archie Hewitt <hewitt.archie@yandex.ru>
2021-12-15 02:38:54 +0300

    Readed proteomics

--- scripts/python/meta/tasks/proteomics/003_dnam_genes_filtering.py
+++ scripts/python/meta/tasks/proteomics/003_dnam_genes_filtering.py
@@ -4,7 +4,16 @@ from scipy.stats import pearsonr, spearmanr, mannwhitneyu
 from scripts.python.routines.manifest import get_manifest
 from scripts.python.EWAS.routines.correction import correct_pvalues
 from tqdm import tqdm
+import re
 
+def get_genes_list(df: pd.DataFrame, col: str, emptys):
+    genes_raw = df.loc[:, col].values
+    genes_all = set()
+    for genes_row in genes_raw:
+        if genes_row not in emptys:
+            genes = set(re.split(r'[.;]+', genes_row))
+            genes_all.update(genes)
+    return list(genes_all)
 
 path = f"E:/YandexDisk/Work/pydnameth/datasets"
 datasets_info = pd.read_excel(f"{path}/datasets.xlsx", index_col='dataset')
@@ -22,14 +31,26 @@ corr_type = 'fdr_bh' # 'bonferroni'
 thld_age = 0.01
 thld_sex = 0.01
 
-def get_genes_list(df: pd.DataFrame):
-    genes_raw = df.loc[:, 'Gene'].values
-    genes_all = set()
-    for genes_row in genes_raw:
-        if genes_row != 'non-genic':
-            genes = set(genes_row.split(';'))
-            genes_all.update(genes)
-    return list(genes_all)
+SS_lists = {}
+AA_lists = {}
+SSAA_lists = {}
+
+proteomic_path = f"E:/YandexDisk/Work/pydnameth/methylation_and_proteomic"
+t1 = pd.read_excel(f"{proteomic_path}/proteomic_data/T1.xlsx", index_col='ID')
+t4 = pd.read_excel(f"{proteomic_path}/proteomic_data/T4.xlsx", index_col='ID')
+prot = pd.merge(t1, t4, left_index=True, right_index=True)
+SS_prot = prot.loc[prot['q.Sex'] < 0.05, :]
+SS_prot_genes = get_genes_list(SS_prot, 'EntrezGeneSymbol', [np.nan])
+SS_lists['Proteomic'] = SS_prot_genes
+print(f"Proteomic SS genes: {len(SS_prot_genes)}")
+AA_prot = prot.loc[prot['q.Age'] < 0.05, :]
+AA_prot_genes = get_genes_list(AA_prot, 'EntrezGeneSymbol', [np.nan])
+AA_lists['Proteomic'] = AA_prot_genes
+print(f"Proteomic AA genes: {len(AA_prot_genes)}")
+SSAA_prot = prot.loc[(prot['q.Sex'] < 0.05) & (prot['q.Age'] < 0.05), :]
+SSAA_prot_genes = get_genes_list(SSAA_prot, 'EntrezGeneSymbol', [np.nan])
+SSAA_lists['Proteomic'] = SSAA_prot_genes
+print(f"Proteomic SS genes: {len(SSAA_prot_genes)}")
 
 for tissue in tissues:
     tmp_path = f"{path_save}/{tissue}"
@@ -39,15 +60,18 @@ for tissue in tissues:
     stats = pd.read_pickle(f"{tmp_path}/stats.pkl")
 
     AA = stats.loc[(stats[f"{age_corr}_pval_{corr_type}"] < thld_age), :]
-    AA_genes = get_genes_list(AA)
+    AA_genes = get_genes_list(AA, 'Gene', ['non-genic'])
+    AA_lists[tissue] = AA_genes
     print(f"{tissue} AA genes: {len(AA_genes)}")
 
     SS = stats.loc[(stats[f"mannwhitney_pval_{corr_type}"] < thld_sex), :]
-    SS_genes = get_genes_list(SS)
+    SS_genes = get_genes_list(SS, 'Gene', ['non-genic'])
+    SS_lists[tissue] = SS_genes
     print(f"{tissue} SS genes: {len(SS_genes)}")
 
     SSAA = SS.loc[(SS[f"{age_corr}_pval_{corr_type}"] < thld_age), :]
-    SSAA_genes = get_genes_list(SSAA)
+    SSAA_genes = get_genes_list(SSAA, 'Gene', ['non-genic'])
+    SSAA_lists[tissue] = SSAA_genes
     print(f"{tissue} SSAA genes: {len(SSAA_genes)}")
 
 
