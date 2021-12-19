7fb7897cc59a11ce1d86d10e66be0290c7bcaa82
Archie Hewitt <hewitt.archie@yandex.ru>
2021-12-20 02:30:50 +0300

    Fixed unn_gse_specific

--- scripts/python/meta/tasks/unn_dataset_specific/001_calc_stats_and_aa.py
+++ scripts/python/meta/tasks/unn_dataset_specific/001_calc_stats_and_aa.py
@@ -24,8 +24,10 @@ import statsmodels.formula.api as smf
 path = f"E:/YandexDisk/Work/pydnameth/datasets"
 datasets_info = pd.read_excel(f"{path}/datasets.xlsx", index_col='dataset')
 
+gse_dataset = 'GSE152026'
+
 folder_name = f"unn_dataset_specific"
-path_save = f"{path}/meta/tasks/{folder_name}"
+path_save = f"{path}/meta/tasks/{folder_name}/{gse_dataset}"
 Path(f"{path_save}/figs/CpG").mkdir(parents=True, exist_ok=True)
 Path(f"{path_save}/figs/DNAmAge").mkdir(parents=True, exist_ok=True)
 
@@ -38,7 +40,7 @@ num_cpgs_to_plot = 10
 pheno_all = pd.DataFrame(columns=target_features + ['Dataset'] + calc_features)
 pheno_all.index.name = 'subject_id'
 
-datasets = ['GSEUNN', 'GSE152026']
+datasets = ['GSEUNN'] + [gse_dataset]
 for d_id, dataset in enumerate(datasets):
     platform = datasets_info.loc[dataset, 'platform']
     manifest = get_manifest(platform)
@@ -88,16 +90,18 @@ for d_id, dataset in enumerate(datasets):
         betas_all = betas_all.merge(betas, how='inner', left_index=True, right_index=True)
 
 print(f"Number of remaining subjects: {pheno_all.shape[0]}")
-
 betas_all = betas_all.T
 betas_all.index.name = "subject_id"
 
 pheno_all, betas_all = get_pheno_betas_with_common_subjects(pheno_all, betas_all)
 
+platform = datasets_info.loc['GSEUNN', 'platform']
+manifest = get_manifest(platform)
+
 pheno_gse = pheno_all.loc[pheno_all['Dataset'] != 'GSEUNN', :]
 pheno_unn = pheno_all.loc[pheno_all['Dataset'] == 'GSEUNN', :]
 
-metrics = ['R2', 'R2_adj', 'mw_stat', 'mw_pval']
+metrics = ['R2', 'R2_adj', 'mw_stat_acc', 'mw_pval_acc', 'mw_stat_diff', 'mw_pval_diff']
 res_dict = {"metric": calc_features}
 for m in metrics:
     res_dict[m] = np.zeros(len(calc_features))
@@ -108,28 +112,39 @@ for f_id, f in enumerate(calc_features):
     res_dict['R2_adj'][f_id] = reg.rsquared_adj
 
     pheno_all[f"{f}Acc"] = pheno_all[f] - reg.predict(pheno_all)
+    pheno_all[f"{f}Diff"] = pheno_all[f] - pheno_all["Age"]
     pheno_gse = pheno_all.loc[pheno_all['Dataset'] != 'GSEUNN', :]
     pheno_unn = pheno_all.loc[pheno_all['Dataset'] == 'GSEUNN', :]
 
     scatter = go.Figure()
-    add_scatter_trace(scatter, pheno_gse["Age"].values, pheno_gse[f].values, "GSE152026")
+    add_scatter_trace(scatter, [0, 100], [0, 100], "", "lines")
+    add_scatter_trace(scatter, pheno_gse["Age"].values, pheno_gse[f].values, gse_dataset)
     add_scatter_trace(scatter, pheno_gse["Age"].values, reg.fittedvalues.values, "", "lines")
     add_scatter_trace(scatter, pheno_unn["Age"].values, pheno_unn[f].values, "UNN")
     add_layout(scatter, "Age", f, "")
-    scatter.update_layout({'colorway': ['blue', 'blue', 'red']})
+    scatter.update_layout({'colorway': ['black', 'blue', 'blue', 'red']})
     save_figure(scatter, f"{path_save}/figs/DNAmAge/scatter_Age_{f}")
 
     stat, pval = mannwhitneyu(pheno_gse[f"{f}Acc"].values, pheno_unn[f"{f}Acc"].values)
-
-    res_dict['mw_stat'][f_id] = stat
-    res_dict['mw_pval'][f_id] = pval
-
+    res_dict['mw_stat_acc'][f_id] = stat
+    res_dict['mw_pval_acc'][f_id] = pval
     vio = go.Figure()
-    add_violin_trace(vio, pheno_gse[f"{f}Acc"].values, "GSE152026")
+    add_violin_trace(vio, pheno_gse[f"{f}Acc"].values, gse_dataset)
     add_violin_trace(vio, pheno_unn[f"{f}Acc"].values, "UNN")
     add_layout(vio, "", f"{f} acceleration", f"Mann-Whitney p-value: {pval:0.4e}")
     vio.update_layout({'colorway': ['blue', 'red']})
     save_figure(vio, f"{path_save}/figs/DNAmAge/vio_{f}Acc")
+
+    stat, pval = mannwhitneyu(pheno_gse[f"{f}Diff"].values, pheno_unn[f"{f}Diff"].values)
+    res_dict['mw_stat_diff'][f_id] = stat
+    res_dict['mw_pval_diff'][f_id] = pval
+    vio = go.Figure()
+    add_violin_trace(vio, pheno_gse[f"{f}Diff"].values, gse_dataset)
+    add_violin_trace(vio, pheno_unn[f"{f}Diff"].values, "UNN")
+    add_layout(vio, "", f"{f} - Age", f"Mann-Whitney p-value: {pval:0.4e}")
+    vio.update_layout({'colorway': ['blue', 'red']})
+    save_figure(vio, f"{path_save}/figs/DNAmAge/vio_{f}Diff")
+
 res_df = pd.DataFrame(res_dict)
 res_df.set_index("metric", inplace=True)
 res_df.to_excel(f"{path_save}/calc_features.xlsx", index=True)
@@ -143,10 +158,10 @@ df_gse = df.loc[df['Dataset'] != 'GSEUNN', :]
 
 fig = go.Figure()
 add_histogram_trace(fig, df_unn['Age'].values, f"UNN ({df_unn.shape[0]})", 5.0)
-add_histogram_trace(fig, df_gse['Age'].values, f"GSE152026 ({df_gse.shape[0]})", 5.0)
+add_histogram_trace(fig, df_gse['Age'].values, f"{gse_dataset} ({df_gse.shape[0]})", 5.0)
 add_layout(fig, "Age", "Count", "")
 fig.update_layout(colorway = ['blue', 'red'], barmode = 'overlay')
-save_figure(fig, f"{path_save}/histogram")
+save_figure(fig, f"{path_save}/figs/histogram_Age")
 
 cpgs = betas_all.columns.values
 result = {'CpG': cpgs}
@@ -159,14 +174,14 @@ metrics = ['mw_stat', 'mw_pval', 'dataset_pval']
 for m in metrics:
     result[m] = np.zeros(len(cpgs))
 
-for cpg_id, cpg in tqdm(enumerate(cpgs), desc=f'Mann-Whitney', total=len(cpgs)):
+for cpg_id, cpg in tqdm(enumerate(cpgs), desc=f'CpGs', total=len(cpgs)):
     result['Gene'][cpg_id] = manifest.loc[cpg, 'Gene']
     mannwhitney_stat, mannwhitney_pval = mannwhitneyu(df_unn[cpg].values, df_gse[cpg].values)
     result['mw_stat'][cpg_id] = mannwhitney_stat
     result['mw_pval'][cpg_id] = mannwhitney_pval
 
     formula = f"Age + C(Dataset) + C(Sex) + Age"
-    term = f"C(Dataset)[T.GSE152026]"
+    term = f"C(Dataset)[T.GSEUNN]"
     reg = smf.ols(formula=f"{cpg} ~ {formula}", data=df).fit()
     pvalues = dict(reg.pvalues)
     result[f"dataset_pval"][cpg_id] = pvalues[term]
@@ -188,13 +203,13 @@ result.to_pickle(f"{path_save}/CpGs.pkl")
 # for cpg_id, (cpg, row) in enumerate(to_plot.iterrows()):
 #     fig = go.Figure()
 #     add_box_trace(fig, df_unn[cpg].values, 'UNN')
-#     add_box_trace(fig, df_gse[cpg].values, 'GSE152026')
+#     add_box_trace(fig, df_gse[cpg].values, gse_dataset)
 #     add_layout(fig, '', "Methylation Level", f"{cpg} ({manifest.loc[cpg, 'Gene']}): {row['mw_pval_fdr_bh']:0.4e}")
 #     fig.update_layout({'colorway': ['blue', "red"]})
 #     save_figure(fig, f"{path_save}/figs/cpgs/{cpg_id}_{cpg}_box")
 #     fig = go.Figure()
 #     add_violin_trace(fig, df_unn[cpg].values, 'UNN')
-#     add_violin_trace(fig, df_gse[cpg].values, 'GSE152026')
+#     add_violin_trace(fig, df_gse[cpg].values, gse_dataset)
 #     add_layout(fig, '', "Methylation Level", f"{cpg} ({manifest.loc[cpg, 'Gene']}): {row['mw_pval_fdr_bh']:0.4e}")
 #     fig.update_layout({'colorway': ['blue', 'red']})
 #     save_figure(fig, f"{path_save}/figs/cpgs/{cpg_id}_{cpg}_vio")
