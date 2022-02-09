842b2ed2aec89ad05dbcbd87d527caee8cead832
Archie Hewitt <hewitt.archie@yandex.ru>
2022-02-09 15:19:51 +0300

    Fixed missed chars, support for missing GEOparse data

--- scripts/python/GEO/002_gse_table.py
+++ scripts/python/GEO/002_gse_table.py
@@ -9,6 +9,17 @@ import pickle
 import os
 import re
 
+def process_characteristics_ch1(gse_df):
+    to_remove = [';', ': ']
+    remover = re.compile('|'.join(map(re.escape, to_remove)))
+    for gsm, row in gse_df.iterrows():
+        tmp = re.split(r'(;*[a-zA-Z0-9 _]+: )', row['characteristics_ch1'])
+        tmp = [x for x in tmp if x]
+        pairs = dict(zip(tmp[::2], tmp[1::2]))
+        for char_raw, value in pairs.items():
+            char = remover.sub('', char_raw)
+            gse_df.at[gsm, char] = value
+
 
 GPL = 'GPL13534'
 n_gses = 200
@@ -62,38 +73,35 @@ for gse_id, gse in enumerate(gses[0:n_gses]):
             continue
         break
     gse_df_2 = gse_data.phenotype_data
-    gse_df_2.replace('NONE', pd.NA, inplace=True)
-    gse_df_2.index.name = 'gsm'
-    gse_df_2 = gse_df_2.loc[(gse_df_2['platform_id'] == GPL), :]
-    is_index_equal = set(gse_df_1.index) == set(gse_df_2.index)
-    gses_df.at[gse, 'Count'] = gse_df_2.shape[0]
-    if not is_index_equal:
-        print("Index is not equal in GEOmetadb and GEOparse")
-        print(f"gse_df_2 full: {gse_df_2.shape[0]}")
-        print(f"gse_df_1 full: {gse_df_1.shape[0]}")
-        unique_chars = set()
+    GEOparse_data_empy = gse_df_2.empty
+    if not GEOparse_data_empy:
+        gse_df_2.replace('NONE', pd.NA, inplace=True)
+        gse_df_2.index.name = 'gsm'
+        gse_df_2 = gse_df_2.loc[(gse_df_2['platform_id'] == GPL), :]
+        is_index_equal = set(gse_df_1.index) == set(gse_df_2.index)
+        gses_df.at[gse, 'Count'] = gse_df_2.shape[0]
+        if not is_index_equal:
+            print("Index is not equal in GEOmetadb and GEOparse")
+            print(f"gse_df_2 full: {gse_df_2.shape[0]}")
+            print(f"gse_df_1 full: {gse_df_1.shape[0]}")
+            unique_chars = set()
+        else:
+            print("Index is equal")
+            gse_df_1 = gse_df_1.loc[gse_df_2.index, :]
+            gse_df_2 = pd.merge(gse_df_2, gse_df_1['characteristics_ch1'], left_index=True, right_index=True)
+            unique_chars = set.union(*gse_df_2['characteristics_ch1'].str.findall('([a-zA-Z0-9 _]+):').apply(set).to_list())
+            process_characteristics_ch1(gse_df_2)
     else:
-        print("Index is equal")
-        gse_df_1 = gse_df_1.loc[gse_df_2.index, :]
-        gse_df_2 = pd.merge(gse_df_2, gse_df_1['characteristics_ch1'], left_index=True, right_index=True)
-        to_remove = [';', ': ']
-        remover = re.compile('|'.join(map(re.escape, to_remove)))
+        print("No data from GEOparse")
+        gse_df_2 = gse_df_1
         unique_chars = set.union(*gse_df_2['characteristics_ch1'].str.findall('([a-zA-Z0-9 _]+):').apply(set).to_list())
-        for gsm, row in gse_df_2.iterrows():
-            tmp = re.split(r'(;*[a-zA-Z0-9 _]+: )', row['characteristics_ch1'])
-            tmp = [x for x in tmp if x]
-            pairs = dict(zip(tmp[::2], tmp[1::2]))
-            for char_raw, value in pairs.items():
-                char = remover.sub('', char_raw)
-                gse_df_2.at[gsm, char] = value
+        process_characteristics_ch1(gse_df_2)
 
     chars_cols = gse_df_2.columns.values[gse_df_2.columns.str.startswith('characteristics_ch1.')]
     r = re.compile(r"characteristics_ch1.\d*.(.*)")
     remaining_chars = [r.findall(x)[0] for x in chars_cols]
     gses_df.at[gse, 'characteristics_ch1'] = unique_chars.union(set(remaining_chars))
     gses_df.at[gse, 'source_name_ch1'] = gse_df_2['source_name_ch1'].unique()
-    for char in unique_chars:
-        gse_df_2[char] = pd.NA
 
     if not gse_df_2['supplementary_file'].isnull().all():
         gses_df.at[gse, 'raw_files_exist'] = True
@@ -107,6 +115,9 @@ for gse_id, gse in enumerate(gses[0:n_gses]):
     else:
         gses_df.at[gse, 'raw_files_exist'] = False
 
-    gse_df_2.to_excel(f"{path}/{GPL}/{gse_id}_{gse}/gse_GEOparse.xlsx", index=True)
+    if GEOparse_data_empy:
+        gse_df_2.to_excel(f"{path}/{GPL}/{gse_id}_{gse}/gse_GEOmetadb.xlsx", index=True)
+    else:
+        gse_df_2.to_excel(f"{path}/{GPL}/{gse_id}_{gse}/gse_GEOparse.xlsx", index=True)
 
 gses_df.to_excel(f"{path}/{GPL}/gses.xlsx", index=True)
