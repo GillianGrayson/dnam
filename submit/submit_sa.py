c7e6d6e187d2a315aef3f440dc04f95e7985f883
Archie Hewitt <hewitt.archie@yandex.ru>
2022-03-26 12:19:55 +0300

    removed duplications from submit

--- submit/submit_sa.py
+++ submit/submit_sa.py
@@ -2,15 +2,20 @@ import os
 
 model_sa = 'catboost'
 
-catboost_learning_rate = [0.05, 0.005]
-catboost_depth = [6, 8]
-catboost_min_data_in_leaf = [1, 2, 5]
-catboost_max_leaves = [31, 63]
+catboost_learning_rate = [0.01]
+catboost_depth = [6]
+catboost_min_data_in_leaf = [2]
+catboost_max_leaves = [31]
+
+base_dir = "/common/home/yusipov_i/data/dnam/datasets/meta/GPL13534_Blood/Schizophrenia"
+in_dim = 200
 
 if model_sa == 'catboost':
     args = f"--multirun " \
            f"logger=csv " \
+           f"base_dir={base_dir} " \
            f"model_sa={model_sa} " \
+           f"in_dim={in_dim} " \
            f"experiment=unn/dnam/multiclass/trn_val_tst/sa " \
            f"catboost.learning_rate={','.join(str(x) for x in catboost_learning_rate)} " \
            f"catboost.depth={','.join(str(x) for x in catboost_depth)} " \
