86b9c63daf9b04755f78547179b89e8f670b4bb3
Archie Hewitt <hewitt.archie@yandex.ru>
2022-03-26 14:17:50 +0300

    added submits for pl

---
+++ submit/submit_pl.py
@@ -0,0 +1,35 @@
+import os
+
+model_pl = 'tabnet'
+
+tabnet_n_d_n_a = [8, 16]
+tabnet_n_steps = [3]
+tabnet_gamma = [1.3, 1.7]
+tabnet_n_independent = [1]
+tabnet_n_shared = [2]
+tabnet_optimizer_lr = [0.01, 0.005, 0.001]
+tabnet_optimizer_weight_decay = [0.0]
+tabnet_scheduler_step_size = [50]
+tabnet_scheduler_gamma = [0.9]
+
+base_dir = "/common/home/yusipov_i/data/dnam/datasets/meta/GPL13534_Blood/Schizophrenia"
+in_dim = 200
+
+if model_pl == 'tabnet':
+    args = f"--multirun " \
+           f"logger=csv " \
+           f"base_dir={base_dir} " \
+           f"experiment=dnam/multiclass/trn_val_tst/tabnetpl " \
+           f"model._target_=src.models.dnam.tabnet.TabNetModel " \
+           f"model.input_dim={in_dim}" \
+           f"model.n_d_n_a={','.join(str(x) for x in tabnet_n_d_n_a)} " \
+           f"model.n_steps={','.join(str(x) for x in tabnet_n_steps)} " \
+           f"model.gamma={','.join(str(x) for x in tabnet_gamma)} " \
+           f"model.n_independent={','.join(str(x) for x in tabnet_n_independent)} "\
+           f"model.n_shared={','.join(str(x) for x in tabnet_n_shared)} " \
+           f"model.optimizer_lr={','.join(str(x) for x in tabnet_optimizer_lr)} " \
+           f"model.optimizer_weight_decay={','.join(str(x) for x in tabnet_optimizer_weight_decay)} " \
+           f"model.scheduler_step_size={','.join(str(x) for x in tabnet_scheduler_step_size)} " \
+           f"model.scheduler_gamma={','.join(str(x) for x in tabnet_scheduler_gamma)} "
+
+os.system(f"sbatch run_multiclass_trn_val_tst_pl.sh \"{args}\"")
