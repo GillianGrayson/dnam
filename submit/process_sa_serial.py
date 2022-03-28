02d111dcc583d7308700f2c8d0187444eff65dc1
Archie Hewitt <hewitt.archie@yandex.ru>
2022-03-29 02:12:28 +0300

    trying to process with test

--- submit/process_sa_serial.py
+++ submit/process_sa_serial.py
@@ -8,6 +8,7 @@ from scripts.python.routines.plot.save import save_figure
 from scripts.python.routines.plot.scatter import add_scatter_trace
 from scripts.python.routines.plot.layout import add_layout
 import plotly.express as px
+import re
 
 
 model_sa = 'catboost'
@@ -26,17 +27,23 @@ metrics = {
     "auroc_weighted": "AUROC",
 }
 optimized_metric = "accuracy_weighted"
+optimized_part = "test"
 direction = "max"
+
+parts = ['train', 'val', 'test']
 
 baseline_fn = f"{base_dir}/harmonized/models/baseline/dnam_harmonized_multiclass_Status_trn_val_tst_{model_sa}/runs/2022-03-27_01-51-36/metrics_val_best_0013.xlsx"
 baseline_metrics_df = pd.read_excel(baseline_fn, index_col="metric")
 
-metrics_global_df = pd.DataFrame(index=n_feats, columns=list(metrics.keys()) + ['file'])
+metrics_global_df = pd.DataFrame(
+    index=n_feats,
+    columns=[x + f"_train" for x in list(metrics.keys())] + [x + f"_val" for x in list(metrics.keys())] + [x + f"_test" for x in list(metrics.keys())] + ['file']
+)
 metrics_global_df.index.name = "n_feat"
 for n_feat in n_feats:
     print(n_feat)
-    project_name = f'dnam_harmonized_multiclass_Status_trn_val_tst_{model_sa}_{n_feat}'
-    files = glob(f"{models_dir}/{project_name}/multiruns/*/*/metrics_val_best_*.xlsx")
+    project_name = f'multiclass_Status_trn_val_tst_{model_sa}_{n_feat}'
+    files = glob(f"{models_dir}/{project_name}/multiruns/*/*/metrics_{optimized_part}_best_*.xlsx")
 
     if len(files) != num_realizations:
         print(len(files))
@@ -48,40 +55,49 @@ for n_feat in n_feats:
     metrics_local_df = pd.DataFrame(index=files, columns=[optimized_metric])
     for file in files:
         df = pd.read_excel(file, index_col="metric")
-        metrics_local_df.at[file, optimized_metric] = df.at[optimized_metric, "val"]
+        metrics_local_df.at[file, optimized_metric] = df.at[optimized_metric, optimized_part]
     metrics_local_df.sort_values([optimized_metric], ascending=[False if direction == "max" else True], inplace=True)
     best_file = metrics_local_df.index.values[0]
     metrics_global_df.at[n_feat, 'file'] = best_file
     print(best_file)
-    df = pd.read_excel(best_file, index_col="metric")
-    for m in metrics:
-        metrics_global_df.at[n_feat, m] = df.at[m, "val"]
 
-Path(f"{models_dir}/{model_sa}_iterative").mkdir(parents=True, exist_ok=True)
-metrics_global_df.to_excel(f"{models_dir}/{model_sa}_iterative/metrics.xlsx", index=True)
-for m in metrics:
-    fig = go.Figure()
-    add_scatter_trace(fig, metrics_global_df.index.values, metrics_global_df.loc[:, m], f"", mode='lines+markers')
-    fig.add_trace(
-        go.Scatter(
-            x=[n_feats[0], n_feats[-1]],
-            y=[baseline_metrics_df.at[m, "val"], baseline_metrics_df.at[m, "val"]],
-            showlegend=False,
-            mode='lines',
-            line=dict(color='black', width=3, dash='dash')
+    head, tail = os.path.split(best_file)
+    search_string = 'metrics_.*_best_(\d*).xlsx'
+    search_split_id = re.search(search_string, tail)
+    split_id = search_split_id.group(1)
+    print(split_id)
+
+    for part in parts:
+        df = pd.read_excel(f"{head}/metrics_{part}_best_{split_id}.xslx", index_col="metric")
+        for metric in metrics:
+            metrics_global_df.at[n_feat, f"{metric}_{part}"] = df.at[metric, part]
+
+Path(f"{models_dir}/iterative/{model_sa}").mkdir(parents=True, exist_ok=True)
+metrics_global_df.to_excel(f"{models_dir}/iterative/{model_sa}/metrics.xlsx", index=True)
+for p in parts:
+    for m in metrics:
+        fig = go.Figure()
+        add_scatter_trace(fig, metrics_global_df.index.values, metrics_global_df.loc[:, f"{m}_{p}"], f"", mode='lines+markers')
+        # fig.add_trace(
+        #     go.Scatter(
+        #         x=[n_feats[0], n_feats[-1]],
+        #         y=[baseline_metrics_df.at[m, optimized_part], baseline_metrics_df.at[m, optimized_part]],
+        #         showlegend=False,
+        #         mode='lines',
+        #         line=dict(color='black', width=3, dash='dash')
+        #     )
+        # )
+        add_layout(fig, f"Number of features in model", f"{metrics[m]}", "")
+        fig.update_layout({'colorway': ['red', 'black']})
+        fig.update_layout(legend_font_size=20)
+        fig.update_layout(legend={'itemsizing': 'constant'})
+        fig.update_layout(
+            margin=go.layout.Margin(
+                l=130,
+                r=20,
+                b=80,
+                t=20,
+                pad=0
+            )
         )
-    )
-    add_layout(fig, f"Number of features in model", f"{metrics[m]}", "")
-    fig.update_layout({'colorway': ['red', 'black']})
-    fig.update_layout(legend_font_size=20)
-    fig.update_layout(legend={'itemsizing': 'constant'})
-    fig.update_layout(
-        margin=go.layout.Margin(
-            l=130,
-            r=20,
-            b=80,
-            t=20,
-            pad=0
-        )
-    )
-    save_figure(fig, f"{models_dir}/{model_sa}_iterative/{m}")
+        save_figure(fig, f"{models_dir}/iterative/{model_sa}/{m}_{p}")
