032e83c040aad670bd7a6b1af7e64ac3c5a2984e
Archie Hewitt <hewitt.archie@yandex.ru>
2022-06-07 17:40:40 +0300

    Pearson correlation and missed values

--- scripts/python/routines/plot/save.py
+++ scripts/python/routines/plot/save.py
@@ -1,10 +1,13 @@
 import plotly
 import time
+import plotly.offline as py
+import plotly.graph_objs as go
+import plotly.io as pio
 
 def save_figure(fig, fn, width=800, height=600, scale=2):
+    #py.plot(fig, filename=f"{fn}.png", include_mathjax='cdn')
+    #py.plot(fig, filename=f"{fn}.pdf", include_mathjax='cdn')
     fig.write_image(f"{fn}.png")
-    fig.write_image(f"{fn}.pdf", format="pdf")
-    #time.sleep(2)
     fig.write_image(f"{fn}.pdf", format="pdf")
     #fig.write_image(f"{fn}.pdf", format="pdf")
     #plotly.io.write_image(fig, f"{fn}.png", width=width, height=height, scale=scale)
