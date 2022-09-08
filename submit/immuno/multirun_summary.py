6d878943f3ff8161656787eadb6011b215b9023d
Igor Yusipov <hewitt.archie@yandex.ru>
2022-09-08 14:17:39 +0300

    Ready for run on cluster

--- submit/immuno/multirun_summary.py
+++ submit/immuno/multirun_summary.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from glob import glob
 import os
+from omegaconf import OmegaConf
 
 path_models = f"/common/home/yusipov_i/data/unn/immuno/models"
 
@@ -12,16 +13,31 @@ path_load = f"{path_models}/immuno_trn_val_{model}/multiruns/{run_time}"
 files = glob(f"{path_load}/*/metrics_val_best_*.xlsx")
 
 df_tmp = pd.read_excel(files[0], index_col="metric")
-df_res = pd.DataFrame(index=files, columns=[m + "_trn" for m in df_tmp.index.values] + [m + "_val" for m in df_tmp.index.values])
+head, tail = os.path.split(files[0])
+cfg = OmegaConf.load(f"{head}/.hydra/overrides.yaml")
+params = []
+for param_pair in cfg:
+    param, val = param_pair.split('=')
+    params.append(param)
+df_res = pd.DataFrame(index=files, columns=[m + "_trn" for m in df_tmp.index.values] + [m + "_val" for m in df_tmp.index.values] + params)
 for file in files:
+    # Validation
     df_val = pd.read_excel(file, index_col="metric")
     for metric in df_val.index.values:
         df_res.at[file, metric + "_val"] = df_val.at[metric, "val"]
+
+    # Train
     head, tail = os.path.split(file)
     tail = tail.replace('val', 'trn')
     df_trn = pd.read_excel(f"{head}/{tail}", index_col="metric")
     for metric in df_trn.index.values:
         df_res.at[file, metric + "_trn"] = df_trn.at[metric, "trn"]
+
+    # Params
+    cfg = OmegaConf.load(f"{head}/.hydra/overrides.yaml")
+    for param_pair in cfg:
+        param, val = param_pair.split('=')
+        df_res.at[file, param] = val
 
 first_columns = [
     'mean_absolute_error_trn',
--- submit/immuno/submit.py
+++ submit/immuno/submit.py
@@ -4,15 +4,15 @@ segment = 'a100'
 
 base_dir = f"/common/home/yusipov_i/data/unn/immuno"
 
-max_epochs = 5000
-patience = 250
+max_epochs = 1000
+patience = 100
 progress_bar_refresh_rate = 0
 
 model_dict = {
-    'elastic_net': ('elastic_net', 'stand_alone'),
-    'xgboost': ('xgboost', 'stand_alone'),
-    'catboost': ('catboost', 'stand_alone'),
-    'lightgbm': ('lightgbm', 'stand_alone'),
+    #'elastic_net': ('elastic_net', 'stand_alone'),
+    #'xgboost': ('xgboost', 'stand_alone'),
+    #'catboost': ('catboost', 'stand_alone'),
+    #'lightgbm': ('lightgbm', 'stand_alone'),
     'widedeep_tab_mlp': ('widedeep_tab_mlp', 'pytorch'),
     'widedeep_tab_net': ('widedeep_tab_net', 'pytorch'),
     'widedeep_saint': ('widedeep_saint', 'pytorch'),
@@ -20,26 +20,26 @@ model_dict = {
     'pytorch_tabular_autoint': ('pytorch_tabular_autoint', 'pytorch'),
     'pytorch_tabular_tabnet': ('pytorch_tabular_tabnet', 'pytorch'),
     'pytorch_tabular_node': ('pytorch_tabular_node', 'pytorch'),
-    'nbm_spam_spam': ('nbm_spam_spam', 'pytorch'),
-    'nbm_spam_nam': ('nbm_spam_nam', 'pytorch'),
-    'nbm_spam_nbm': ('nbm_spam_nbm', 'pytorch'),
+    # 'nbm_spam_spam': ('nbm_spam_spam', 'pytorch'),
+    # 'nbm_spam_nam': ('nbm_spam_nam', 'pytorch'),
+    # 'nbm_spam_nbm': ('nbm_spam_nbm', 'pytorch'),
     'danet': ('danet', 'pytorch'),
     'nam': ('nam', 'pytorch'),
-    'arm_net_models_armnet': ('arm_net_models', 'pytorch'),
-    'arm_net_models_armnet_1h': ('arm_net_models', 'pytorch'),
-    'arm_net_models_afn': ('arm_net_models', 'pytorch'),
-    'arm_net_models_gc_arm': ('arm_net_models', 'pytorch'),
-    'arm_net_models_sa_glu': ('arm_net_models', 'pytorch'),
-    'arm_net_models_dcn': ('arm_net_models', 'pytorch'),
-    'arm_net_models_cin': ('arm_net_models', 'pytorch'),
-    'arm_net_models_gcn': ('arm_net_models', 'pytorch'),
-    'arm_net_models_gat': ('arm_net_models', 'pytorch'),
-    'arm_net_models_wd': ('arm_net_models', 'pytorch'),
-    'arm_net_models_ipnn': ('arm_net_models', 'pytorch'),
-    'arm_net_models_kpnn': ('arm_net_models', 'pytorch'),
-    'arm_net_models_nfm': ('arm_net_models', 'pytorch'),
-    'arm_net_models_dcn_plus': ('arm_net_models', 'pytorch'),
-    'arm_net_models_xdfm': ('arm_net_models', 'pytorch'),
+    # 'arm_net_models_armnet': ('arm_net_models', 'pytorch'),
+    # 'arm_net_models_armnet_1h': ('arm_net_models', 'pytorch'),
+    # 'arm_net_models_afn': ('arm_net_models', 'pytorch'),
+    # 'arm_net_models_gc_arm': ('arm_net_models', 'pytorch'),
+    # 'arm_net_models_sa_glu': ('arm_net_models', 'pytorch'),
+    # 'arm_net_models_dcn': ('arm_net_models', 'pytorch'),
+    # 'arm_net_models_cin': ('arm_net_models', 'pytorch'),
+    # 'arm_net_models_gcn': ('arm_net_models', 'pytorch'),
+    # 'arm_net_models_gat': ('arm_net_models', 'pytorch'),
+    # 'arm_net_models_wd': ('arm_net_models', 'pytorch'),
+    # 'arm_net_models_ipnn': ('arm_net_models', 'pytorch'),
+    # 'arm_net_models_kpnn': ('arm_net_models', 'pytorch'),
+    # 'arm_net_models_nfm': ('arm_net_models', 'pytorch'),
+    # 'arm_net_models_dcn_plus': ('arm_net_models', 'pytorch'),
+    # 'arm_net_models_xdfm': ('arm_net_models', 'pytorch'),
 }
 
 for model_name, (model_type, model_framework) in model_dict.items():
