d561fba069b3792576c66450bc0b1fd3b36447a1
Archie Hewitt <hewitt.archie@yandex.ru>
2022-09-11 18:42:01 +0300

    Significance tests for Yakutia

--- scripts/python/preprocessing/from_xlsx_to_pkl.py
+++ scripts/python/preprocessing/from_xlsx_to_pkl.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 
-dataset = "GSE80417"
+dataset = "GSEUNN"
 path = f"E:/YandexDisk/Work/pydnameth/datasets"
 datasets_info = pd.read_excel(f"{path}/datasets.xlsx", index_col='dataset')
 platform = datasets_info.loc[dataset, 'platform']
 
-df = pd.read_excel(f"{path}/{platform}/{dataset}/pheno.xlsx", index_col='subject_id')
-df.to_pickle(f"{path}/{platform}/{dataset}/pheno.pkl")
+df = pd.read_excel(f"{path}/{platform}/{dataset}/pheno_xtd.xlsx", index_col='index')
+df.to_pickle(f"{path}/{platform}/{dataset}/pheno_xtd.pkl")
