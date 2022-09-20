90a7e5bea23caefb87f48548a9319bf4cf09428a
Archie Hewitt <hewitt.archie@yandex.ru>
2022-09-20 14:33:54 +0300

    Preparing for small immuno age

--- submit/immuno/submit_multirun.py
+++ submit/immuno/submit_multirun.py
@@ -2,6 +2,8 @@ import os
 
 segment = 'a100'
 
+features_file = "feats_con"
+
 base_dir = f"/common/home/yusipov_i/data/unn/immuno"
 
 max_epochs = 1000
@@ -12,18 +14,18 @@ model_dict = {
     # 'elastic_net': ('elastic_net', 'stand_alone'),
     # 'xgboost': ('xgboost', 'stand_alone'),
     # 'catboost': ('catboost', 'stand_alone'),
-    # 'lightgbm': ('lightgbm', 'stand_alone'),
+    'lightgbm': ('lightgbm', 'stand_alone'),
     # 'widedeep_tab_mlp': ('widedeep_tab_mlp', 'pytorch'),
-    # 'widedeep_tab_net': ('widedeep_tab_net', 'pytorch'),
-    'widedeep_saint': ('widedeep_saint', 'pytorch'),
-    'widedeep_ft_transformer': ('widedeep_ft_transformer', 'pytorch'),
-    'pytorch_tabular_autoint': ('pytorch_tabular_autoint', 'pytorch'),
+    'widedeep_tab_net': ('widedeep_tab_net', 'pytorch'),
+    # 'widedeep_saint': ('widedeep_saint', 'pytorch'),
+    # 'widedeep_ft_transformer': ('widedeep_ft_transformer', 'pytorch'),
+    # 'pytorch_tabular_autoint': ('pytorch_tabular_autoint', 'pytorch'),
     # 'pytorch_tabular_tabnet': ('pytorch_tabular_tabnet', 'pytorch'),
     # 'pytorch_tabular_node': ('pytorch_tabular_node', 'pytorch'),
     # 'nbm_spam_spam': ('nbm_spam_spam', 'pytorch'),
     # 'nbm_spam_nam': ('nbm_spam_nam', 'pytorch'),
     # 'nbm_spam_nbm': ('nbm_spam_nbm', 'pytorch'),
-    # 'danet': ('danet', 'pytorch'),
+    'danet': ('danet', 'pytorch'),
     # 'nam': ('nam', 'pytorch'),
     # 'arm_net_models_armnet': ('arm_net_models', 'pytorch'),
     # 'arm_net_models_armnet_1h': ('arm_net_models', 'pytorch'),
@@ -54,6 +56,7 @@ for model_name, (model_type, model_framework) in model_dict.items():
            f"max_epochs={max_epochs} " \
            f"patience={patience} " \
            f"print_config=False " \
-           f"base_dir={base_dir} "
+           f"datamodule.feats_con_fn={base_dir}/{features_file}.xlsx" \
+           f"base_dir={base_dir}"
 
     os.system(f"sbatch run_{segment}.sh \"{args}\"")
